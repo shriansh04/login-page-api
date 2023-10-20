9f69c979a5d9625bdfa09d2bde453a72dac0e57b
shriansh04 <agrawal.19@iitj.ac.in>
2023-10-20 09:21:18 +0530

    first commit

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,26 +1,34 @@
 import names from './names.json';
-import React,{useState} from 'react';
+import React, { useState } from 'react';
+
 function Search() {
   const [searchkey, setSearchKey] = useState("");
-  return(
-    <>
-      Search : <input type="search" placeholder="Search here" onChange={(event)=> {
-        setSearchKey(event.target.value);
 
-        }}/>
-        {names.filter((value)=>{
-          if(searchkey === ""){
-            return true
+  return (
+    <>
+      Search: <input
+        type="search"
+        placeholder="Search here"
+        onChange={(event) => {
+          setSearchKey(event.target.value);
+        }}
+      />
+      {names
+        .filter((value) => {
+          if (searchkey === "") {
+            return true;
+          } else if (value.name.toLowerCase().includes(searchkey.toLowerCase())) {
+            return true;
+          } else {
+            return false; 
           }
-          else if(value.name.toLowerCase().includes(searchkey.toLowerCase())){
-            return true
-          }
-        }).map((names)=>{
-          return(
-            <div>{names.name}</div>
-          )
+        })
+        .map((name, index) => {
+          return <div key={index}>{name.name}</div>; // Use 'key' prop with a unique identifier or 'index'.
         })}
     </>
-  )
+  );
 }
+
 export default Search;
+
